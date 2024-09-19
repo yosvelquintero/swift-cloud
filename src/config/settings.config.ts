02af1d02638d6aeebd2515cf8c99fff02aea131e
Yosvel Quintero <yosvelquintero@gmail.com>
2024-09-19 16:02:19 +0700

    feat: Add settings configuration for security, body parsing, CORS, API versioning, global prefix, and global filters
    This commit adds a new file, `settings.config.ts`, which contains the configuration settings for the NestJS application. It includes middleware for security using Helmet, body parsing using JSON and URL encoding, enabling CORS, API versioning using URI, setting a global prefix for all routes, and global filters for error handling. These settings enhance the overall functionality and security of the application.

---
+++ src/config/settings.config.ts
@@ -0,0 +1,64 @@
+import {
+  INestApplication,
+  RequestMethod,
+  ValidationPipe,
+  VersioningType,
+} from '@nestjs/common';
+import { json, urlencoded } from 'express';
+import helmet from 'helmet';
+
+import { ValidationErrorFilter } from '@app/filters';
+
+export const settingsConfig = (prefix: string, app: INestApplication): void => {
+  // Security middleware using Helmet
+  app.use(helmet());
+
+  // Body Parsing Middleware
+  app.use(
+    json({
+      limit: 2 * 1024 * 1024,
+    }),
+  );
+
+  // URL Encoding Middleware
+  app.use(
+    urlencoded({
+      limit: 1 * 1024 * 1024,
+      extended: true,
+    }),
+  );
+
+  // Enable Cross-Origin Resource Sharing
+  app.enableCors();
+
+  // API Versioning
+  app.enableVersioning({
+    type: VersioningType.URI,
+  });
+
+  // Set Global Prefix for all routes
+  app.setGlobalPrefix(prefix, {
+    exclude: [
+      // Exclude Swagger Routes from Global Prefix
+      {
+        path: 'swagger',
+        method: RequestMethod.ALL,
+      },
+    ],
+  });
+
+  // Global Pipes for validation and transformation
+  app.useGlobalPipes(
+    new ValidationPipe({
+      forbidNonWhitelisted: true,
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+      whitelist: true,
+    }),
+  );
+
+  // Global Filters for error handling
+  app.useGlobalFilters(new ValidationErrorFilter());
+};
