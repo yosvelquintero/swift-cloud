8257baa767eb331acce90c1a07e192c6b85f487d
Yosvel Quintero <yosvelquintero@gmail.com>
2024-09-19 16:18:12 +0700

    refactor: Update app.module.ts to include settings configuration and mongoose setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ENV } from './config/constants';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    MongooseModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get<string>(ENV.database.mongodb.mongodbUri),
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
