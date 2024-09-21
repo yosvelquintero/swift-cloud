e6e2986b89178d91c0c855baa71050681ff44950
Yosvel Quintero <yosvelquintero@gmail.com>
2024-09-21 10:39:58 +0700

    feat: Add getMongooseSchemaOptions helper function

---
+++ src/types/interfaces/mongoose-schema-options.ts
@@ -0,0 +1,3 @@
+export interface IGetMongooseSchemaOptionsParams {
+  collection: string;
+}
---
+++ src/utils/mongoose-schema-options.helper.ts
@@ -0,0 +1,15 @@
+import { SchemaOptions } from '@nestjs/mongoose';
+
+import { IGetMongooseSchemaOptionsParams } from '@app/types';
+
+export function getMongooseSchemaOptions(
+  params: IGetMongooseSchemaOptionsParams,
+): SchemaOptions {
+  return {
+    collection: params.collection,
+    timestamps: {
+      createdAt: 'created',
+      updatedAt: 'updated',
+    },
+  };
+}
