3380a725e76d162132c5726ab61ae9b4c058010c
Yosvel Quintero <yosvelquintero@gmail.com>
2024-09-22 17:44:50 +0700

    refactor: Update error logging in main-import-data.ts

--- src/main-import-data.ts
+++ src/main-import-data.ts
@@ -1,3 +1,4 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app.module';
@@ -6,10 +7,11 @@ import { ImportService } from './modules/imports/imports.service';
 async function bootstrap() {
   const app = await NestFactory.createApplicationContext(AppModule);
   const importService = app.get(ImportService);
+  const logger = new Logger('ImportData');
 
   const filePath = process.argv[2];
   if (!filePath) {
-    console.error('Please provide the CSV file path as an argument.');
+    logger.error('Please provide the CSV file path as an argument.');
     process.exit(1);
   }
 
